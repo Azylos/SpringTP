675a066b448160db078361f313dbd4826e39f5f6
Toma <tomabarret@hotmail.com>
2025-05-15 15:10:16 +0400

    Un utilisateur peut valider ses propres tâches.

--- src/main/java/com/dreams/azyl/tp1/Controller/UtilisateurController.java
+++ src/main/java/com/dreams/azyl/tp1/Controller/UtilisateurController.java
@@ -4,6 +4,7 @@ import com.dreams.azyl.tp1.Entity.ConsultationTache;
 import com.dreams.azyl.tp1.Entity.Tache;
 import com.dreams.azyl.tp1.Entity.Utilisateur;
 import com.dreams.azyl.tp1.Repository.ConsultationTacheRepository;
+import com.dreams.azyl.tp1.Repository.TacheRepository;
 import com.dreams.azyl.tp1.Repository.UtilisateurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,8 @@ public class UtilisateurController {
     private UtilisateurRepository utilisateurRepository;
     @Autowired
     private ConsultationTacheRepository consultationTacheRepository;
+    @Autowired
+    private TacheRepository tacheRepository;
 
     @PutMapping("/{id}/inscription")
     private ResponseEntity<String> inscription(@PathVariable Long id) {
@@ -48,5 +51,28 @@ public class UtilisateurController {
                 .filter(t -> "EN_COURS".equalsIgnoreCase(t.getStatut()))
                 .toList();
         return ResponseEntity.ok(enCours);
+    }
+
+    @PutMapping("/{id}/valid/{idTache}")
+    private ResponseEntity<String> validTache(@PathVariable Long id, @PathVariable Long idTache) {
+
+        Utilisateur utilisateur = utilisateurRepository.findById(id).orElse(null);
+        Tache tache = tacheRepository.findById(idTache).orElse(null);
+
+        if (utilisateur == null) {
+            return ResponseEntity.status(404).build();
+        }
+
+        if (tache == null || tache.getStatut().equals("VALIDER")) {
+            return ResponseEntity.status(404).body("Tache introuvable ou déjà valider");
+        }
+
+        if (tache.getUtilisateur().getId().equals(utilisateur.getId())) {
+            tache.setStatut("VALIDER");
+            tacheRepository.save(tache);
+            return ResponseEntity.ok("Tache valider avec succès");
+        } else {
+            return ResponseEntity.status(400).body("L'utilisateur ne possède pas cette tache");
+        }
     }
 }
